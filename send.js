a10fc9ed08f85c60e148e0601f1e3440e072fe8e
yuhsin1006 <yuhsin0@gmail.com>
2017-03-31 02:37:47 +0800

    增加定時傳資料檔案(測試)

--- send.js
+++ send.js
@@ -1,6 +1,8 @@
 // Dependencies:
 // npm install request request-promise-native
 
+
+
 var request = require('request-promise-native')
 request.post('http://120.107.172.236:3000/devices/').then(v => {
   console.dir(JSON.parse(v))
