1ab3cd46b9e7f5a208f30b050f9bcbed18bb07aa
yuhsin1006 <yuhsin0@gmail.com>
2017-04-05 15:19:59 +0800

    修正固定送req

--- request.js
+++ request.js
@@ -1,35 +1,3 @@
-/*******送封包出去 但好像收不到json**********/
-
-/*
-var rp = require('request-promise');
-
-var options = {
-    method: 'POST',
-    uri: 'http://120.107.172.236:3000/devices',
-    body: {
-        serial : 87 ,
-        mac : 12,
-        belongTo : 'yuhsin'
-    },
-    json: true, // Automatically stringifies the body to JSON
-    resolveWithFullResponse: true  
-};
-
-rp(options)
-    .then(function (response) {
-        // POST succeeded...
-        console.log("POST successed!");
-        console.log("Succeeded with status %d", response.statusCode);
-      //  var info = JSON.parse(response);
-        console.log(response);
-
-    })
-    .catch(function (err) {
-        // POST failed...
-        console.log("POST failed......");
-    });
-*/
-/*******收的到json 拆不開**********/
 
 var http = require('http');
 var express = require('express');
@@ -57,20 +25,23 @@ var options = {
  
  var request = require('request');
 
-function callback(error, response, body) {
-  if (!error && response.statusCode == 200) {
+var request = require('request-promise-native')
+var cron = require('cron');
 
-    console.log(body);
-  //  console.log(response.body);
+var cronJob = cron.job("0 * * * * *", function(){
+    // perform operation e.g. GET request http.get() etc.
 
-  //  var info = JSON.parse(body);
-//    console.log(info);
-  //  console.log(info.result + " result ");
-  //  console.log(info.message + " msgs ");
-      console.log(response.statusCode)
-      console.log(response.headers['content-type'])
-  //    console.dir(response.toJSON())
-  }
-}
+    function callback(error, response, body) {
+        if (!error && response.statusCode == 200) {
+
+        console.log(" Result: " + body.result);
+        console.log(" Message:  " + body.message);
  
-request(options, callback);
+        }
+    }
+ 
+    request(options, callback);
+    console.info('cron job completed');
+
+}); 
+cronJob.start();
